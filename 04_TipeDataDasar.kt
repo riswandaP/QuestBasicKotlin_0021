fa79b4e22ace153ccec02f163dc01372eba75a30
riswandaP <riswandaputra03@gmail.com>
2026-09-24 20:13:48 +0700

    Tipe data dasar

---
+++ 04_TipeDataDasar.kt
@@ -0,0 +1,19 @@
+fun main() {
+    // Variabel dideklarasikan tanpa inisialisasi
+    val d: Int
+    // Variabel diberi nilai kemudian
+    d = 3
+    // Tipe eksplisit dan langsung diinisialisasi
+    val e: String = "hello"
+
+    // Contoh tipe dasar lain
+    val umur: Int = 20
+    val tinggi: Double = 170.5
+    val lulus: Boolean = true
+    val huruf: Char = 'A'
+
+    println("d = $d")
+    println("e = $e")
+    println("umur = $umur, tinggi = $tinggi")
+    println("lulus = $lulus, huruf = $huruf")
+}
