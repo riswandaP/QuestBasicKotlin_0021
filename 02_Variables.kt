a230b0cc4da1c35d6745064c47c2e2d31929ce20
riswandaP <riswandaputra03@gmail.com>
2026-09-24 20:01:00 +0700

    Variables

---
+++ 02_Variables.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val popcorn = 5      // read-only, tidak bisa diubah
+    var customers = 10   // bisa diubah
+    customers = 8
+    println("popcorn = $popcorn, customers = $customers")
+}
